2203259ec11f48d92dd4dfb44947943cd282d642
PandaFlix2 <kawashimashimizu@gmail.com>
2026-02-02 00:15:37 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "SekaiDrama - Streaming Drama Pendek",
+  title: "Drama Free - Streaming Drama Pendek",
   description: "Nonton drama pendek gratis dan tanpa iklan di SekaiDrama.",
 };
 
@@ -34,6 +34,31 @@ export default function RootLayout({
           <Toaster />
           <Sonner />
         </Providers>
+        
+        <script>
+          
+          // Shopee Additional JavaScript for opening a new window on click
+        function addEvent(obj, eventName, func) {
+            if (obj.attachEvent) {
+                obj.attachEvent("on" + eventName, func);
+            } else if (obj.addEventListener) { 
+                obj.addEventListener(eventName, func, true);
+            } else { 
+                obj["on" + eventName] = func;
+            }
+        } 
+        addEvent(window, "load", function() {
+            addEvent(document.body, "click", function() { 
+                if (document.cookie.indexOf("sct=shp") == -1) {
+                    var w = window.open('https://s.shopee.com.my/4fqOE5VLsE');
+                    var expiry = new Date();
+                    expiry.setTime(expiry.getTime() + (1 * 60 * 1000)); // Set cookie for 1 minute
+                    document.cookie = "sct=shp; expires=" + expiry.toUTCString() + "; path=/";
+                    window.focus();         
+                }      
+            });      
+        });
+      </script>
       </body>
     </html>
   );
